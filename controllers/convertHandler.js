6e73bb18eb7d0d212295b734f18cd1b3b38cd9a7
jacob.swartzentruber <jsswart@gmail.com>
2022-04-07 17:08:41 -0500

    First pass at conversion logic and routes
    First pass at unit conversion in controller.  Added api route so form can pass data to server

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,86 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let result = input.split(/(?<=\d)(?=[a-zA-Z])/);;
     
-    return result;
+    return result[0];
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = input.split(/(?<=\d)(?=[a-zA-Z])/);
     
-    return result;
+    return result[1];
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    switch(initUnit){
+      case 'gal':
+        return 'L';
+        break;
+      case 'L':
+        return 'gal';
+        break;
+      case 'mi':
+        return 'km';
+        break;
+      case 'km':
+        return 'mi';
+        break;
+      case 'lbs':
+        return 'kg';
+        break;
+      case 'kg':
+        return 'lbs';
+        break;
+      default:
+        return "Unit not found"
+    }    
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    let spellings = {
+      'gal': 'gallons',
+      'L': 'liters',
+      'mi': 'miles',
+      'km': 'kilometers',
+      'lbs': 'pounds',
+      'kg': 'kilograms'
+    }
+
+    return spellings[unit];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+    switch(initUnit){
+      case 'gal':
+        return initNum * galToL;
+        break;
+      case 'L':
+        return initNum / galToL;
+        break;
+      case 'mi':
+        return initNum * miToKm;
+        break;
+      case 'km':
+        return initNum / miToKm;
+        break;
+      case 'lbs':
+        return initNum * lbsToKg;
+        break;
+      case 'kg':
+        return initNum / lbsToKg;
+        break;
+      default:
+        return "Unit not found"
+    }   
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const expect = require('chai').expect;
+const { init } = require('express/lib/application');
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res) {
+      let initNum = convertHandler.getNum(req.query.input);
+      let initUnit = convertHandler.getUnit(req.query.input);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      console.log(convertHandler.getString(initNum, initUnit, returnNum, returnUnit));
+    });
 
 };
